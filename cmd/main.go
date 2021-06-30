54bfee3f1795c98d0e16954c9fc7f57656370b2c
Oliver Benns <oliverbenns@gmail.com>
2021-06-30 22:24:36 +0100

    Add wip

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,28 @@
 package main
 
 import (
+	"image/color"
 	"machine"
 	"time"
+
+	"tinygo.org/x/drivers/ssd1306"
 )
 
 func main() {
+	defer func() {
+		if r := recover(); r != nil {
+			println("an error really happened")
+		}
+	}()
+	machine.I2C0.Configure(machine.I2CConfig{})
+	device := ssd1306.NewI2C(machine.I2C0)
+	device.Configure(ssd1306.Config{
+		Address: 0x3C,
+	})
+
+	device.SetPixel(20, 20, color.RGBA{255, 255, 255, 255})
+
+	device.Display()
 
 	led := machine.D13
 	led.Configure(machine.PinConfig{Mode: machine.PinOutput})
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/oliverbenns/arduino-ssd1306-game-of-life
 
 go 1.16
+
+require tinygo.org/x/drivers v0.16.0 // indirect
